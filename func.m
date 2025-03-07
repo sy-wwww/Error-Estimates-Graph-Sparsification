ba594923694cd165fa041cd863f9a05f6496d274
sy-wwww <syywang@ucdavis.edu>
2025-03-06 16:48:18 -0800

    Update func.m

--- func.m
+++ func.m
@@ -1,5 +1,9 @@
-function f = func(L,S)
+function f = func(L,S,lambda,y)
     f1 = norm(S-L,"fro");
     f2 = svds(S-L,1);
-    f = [f1,f2];
+    n = length(y);
+    betaL = (diag(ones(n,1)) + lambda*L)\y;
+    betaS = (diag(ones(n,1)) + lambda*L)\y;
+    f3 = norm(betaS-betaL);
+    f = [f1,f2,f3];
 end
